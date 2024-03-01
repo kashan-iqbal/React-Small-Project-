62959d8083be65acb4a9e233a9bb44e3e2e31612
kashan-iqbal <kashaniqbal33@gmail.com>
2024-03-01 10:07:18 +0500

    second

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,128 @@
-import { useEffect, useState } from 'react'
-import {io} from "socket.io-client"
+// import { useMemo } from "react";
+// import { useEffect, useState } from "react";
+// import { io } from "socket.io-client";
 
+// function App() {
+//   const socket = useMemo(() => io("http://localhost:3000"), []);
 
+//   const [message, setMessage] = useState("");
+//   const [receve, setReceve] = useState([]);
+//   const [id, setId] = useState("");
+//   const [room, setRoom] = useState("");
+//   const [join, setJoin] = useState("");
 
-function App() {
-  const [message,setMessage] = useState([])
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     socket.emit("message", { message, room });
+//     setMessage("");
+//   };
 
-  const socket = io("http://localhost:3000")
-  useEffect(()=>{
-      socket.on('connect', () => {
-        console.log("Connected to server");
-        console.log(socket.id); // This will log the socket ID once connected
-socket.on("msg",(d)=> console.log(d))
-        socket.on("msg",(d)=>setMessage(d))
-      })
+//   const handleJoin = (e) => {
+//     e.preventDefault();
+//     socket.emit("join", join);
+//     setJoin("");
+//   };
 
-  },[])
+//   useEffect(() => {
+//     socket.on("connect", () => {
+//       console.log("Connected to server");
+//       setId(socket.id);
+//       console.log(socket.id); // This will log the socket ID once connecte
+//     });
+
+//     socket.on("rec-msg", (d) => {
+//       console.log(`i am data ${d}`);
+//       setReceve((prev) => [...prev, d]);
+//     });
+//   }, []);
+
+//   return (
+//     <>
+//       <h1>i ma app socket.io</h1>
+//       <p>{id}</p>
+//       {receve && receve.length
+//         ? receve.map((m, idx) => <p key={idx}>{m}</p>)
+//         : null}
+
+//       <form onSubmit={handleJoin}>
+//         <input
+//           type="text"
+//           onChange={(e) => setJoin(e.target.value)}
+//           value={join}
+//         />
+//         <button type="submit">join</button>
+//       </form>
+
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           onChange={(e) => setMessage(e.target.value)}
+//           value={message}
+//         />
+//         <input
+//           type="text"
+//           onChange={(e) => setRoom(e.target.value)}
+//           value={room}
+//         />
+//         <button type="submit">send</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default App;
+
+// import React, { useEffect } from "react";
+// import { io } from "socket.io-client";
+
+// const App = () => {
+//   const Socket = io(`http://localhost:4000`);
+
+//   // useEffect(() => {
+//   //   Socket.on("connect", () => {
+//   //     console.log(`your are connected to the chat app`);
+//   //   });
+//   //   console.log(`i am use `);
+//   // }, []);
+
+//   useEffect(() => {
+//     console.log(`first`);
+//   }, []);
+
+//   return (
+//     <div>
+//       <select onChange={(e) => console.log(e.target.value)}>
+//         <option>admin</option>
+//         <option>Route</option>
+//         <option>user</option>
+//       </select>
+//       App
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const App = () => {
+  const Socket = io(`http://localhost:4000`);
+console.log(Socket);
+  useEffect(() => {
+    console.log("first");
+  }, []);
 
   return (
-    <>
-    <h1>i ma app socket.io
-      {
-        // message && message.length?message.map((m,idx)=>(<p key={idx}>{m}</p>)) :null
-        // message
-      }
-    </h1>
-    </>
-  )
-}
+    <div>
+      <select onChange={(e) => console.log(e.target.value)}>
+        <option>admin</option>
+        <option>Route</option>
+        <option>user</option>
+      </select>
+      App
+    </div>
+  );
+};
 
-export default App
+export default App;
--- server.js
+++ server.js
@@ -1,39 +1,76 @@
-const express = require("express");
-const { Server } = require("socket.io");
-const { createServer } = require("http");
-const cors = require("cors");
+// const express = require("express");
+// const { Server } = require("socket.io");
+// const { createServer } = require("http");
+// const cors = require("cors");
+// const { log } = require("console");
 
+// const app = express();
+
+// const server = createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   })
+// );
+
+// app.get("/", (req, res) => {
+//   res.send(`<h1>i am web socket</h1>`);
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`user is connected`);
+//   console.log(socket.id);
+
+//   socket.on("message", ({ message, room }) => {
+//     console.log(message);
+//     io.to(room).emit("rec-msg", message);
+//   });
+
+//   socket.on("join", (d) => {
+//     console.log(d)
+//     socket.join(d);
+//   });
+// });
+
+// const PORT = 3000;
+
+// server.listen(PORT, () => {
+//   console.log(`app listing ${PORT}`);
+// });
+
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
 
-const server = createServer(app);
+app.use(cors());
+
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("<h1> I am socket.io</h1>");
+});
+
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
   },
 });
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
 
 io.on("connection", (socket) => {
-  console.log(`user is connected`);
-  console.log(socket.id);
-
- socket.broadcast.emit("msg","i am the web soket now")
-
+  socket.emit("join", "your are connected with this id ", socket.id);
+  console.log(`user is connected ${socket.id}`)
 });
 
-app.get("/", (req, res) => {
-  res.send(`<h1>i am web socket</h1>`);
-});
-
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`app listing ${PORT}`);
-});
+server.listen(4000, () => console.log(`server is runnig on port ${4000}`));
